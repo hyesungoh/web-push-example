016e5992327dc0d861523a8634ada3a669bb09d5
hyesungoh <haesungoh414@gmail.com>
2023-10-25 19:03:45 +0900

    showing compatibility

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const { hasServiceWorker, hasRegistration } = useHasSpec();
+
   return (
     <main>
       <h1>web push example</h1>
+      <p>
+        has serviceWorker : {hasServiceWorker ? "✅" : "❌"}
+      </p>
+      <p>
+        has registration : {hasRegistration ? "✅" : "❌"}
+      </p>
     </main>
   );
+}
+
+function useHasSpec() {
+  const [hasServiceWorker, setHasServiceWorker] = useState(false);
+  const [hasRegistration, setHasRegistration] = useState(false);
+
+  useEffect(() => {
+    setHasServiceWorker("serviceWorker" in navigator);
+
+    async function setRegistration() {
+      const registration = await navigator.serviceWorker.getRegistration();
+      setHasRegistration(Boolean(registration));
+    }
+
+    setRegistration();
+  }, []);
+
+  return { hasServiceWorker, hasRegistration };
 }
